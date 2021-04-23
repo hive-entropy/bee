f30272657637211fa2bc0acbcda35f8584087c9c
Alban Le Moult <alban.lemoult@gmail.com>
2021-04-23 13:34:30 +0200

    Refactor the cannon callback to use templates

--- src/bee.cpp
+++ src/bee.cpp
@@ -29,85 +29,76 @@ void Bee::healthCallback(coap_context_t *context, coap_resource_t *resource, coa
 
 }
 
+
+template<typename T>
+void templatedCannon(coap_session_t *session, Message &inputMessage);
 void Bee::cannonMulCallback(coap_context_t *context, coap_resource_t *resource, coap_session_t *session,
                           coap_pdu_t *request, coap_binary_t *token, coap_string_t *query,
                           coap_pdu_t *response) {
-    /*
-     * Extract the inputMessage.
-     * Get the headers : inputMessage.getHeaders()
-     *
-     * HEADERS: serializedTyped, calculationId, taskId, elementType, steps, inser_at_...,
-     * BODY: list (vector) of matrix (two)
-     *
-     * Get the IDs
-     * Deserialize the matrix (Serializer::unserializeMatrices(string from inputMessage))
-     * Multiply the matrices (the first with the second)
-     * If the IDs are new, store the result matrix of the size of the resulting block.
-     * If IDs are not new, add result to stored matrix
-     * Increment the step counter
-     * If step_counter == max_step,
-         * Then Send a heartbeat inputMessage
-         * Else send matrixMultiplicationResultFragmentMessage with INSERT_AT_X and Y
-     */
     // Extract the inputMessage
     Message inputMessage(session, request);
 
-    // Get the necessary headers
-    std::basic_string<char> calculationId = inputMessage.getHeaders()[Headers::CALCULATION_ID];
-    std::basic_string<char> taskId = inputMessage.getHeaders()[Headers::TASK_ID];
+    // Get the matrix type
     std::basic_string<char> matrixType = inputMessage.getHeaders()[Headers::ELEMENT_TYPE];
+
+    // Call the template method with the corresponding type
+    if (matrixType == typeid(int).name()) {
+        templatedCannon<int>(session, inputMessage);
+    } else if (matrixType == typeid(double).name()) {
+        templatedCannon<double>(session, inputMessage);
+    } else if (matrixType == typeid(float).name()) {
+        templatedCannon<float>(session, inputMessage);
+    }
+}
+
+template<typename T>
+void templatedCannon(coap_session_t *session, Message &inputMessage) {
+    coap_pdu_t *response;// Extract the necessary informations
+    basic_string<char> calculationId = inputMessage.getHeaders()[Headers::CALCULATION_ID];
+    basic_string<char> taskId = inputMessage.getHeaders()[Headers::TASK_ID];
     int startRow = stoi(inputMessage.getHeaders()[Headers::INSERT_AT_X]);
     int startCol = stoi(inputMessage.getHeaders()[Headers::INSERT_AT_Y]);
     int steps = stoi(inputMessage.getHeaders()[Headers::STEPS]);
 
-    std::string globalId = calculationId + taskId;
+    string globalId = calculationId + taskId;
 
-    if (matrixType == typeid(int).name()) {
-        // Deserialize the matrices
-        vector<Matrix<int>> matrices = Serializer::unserializeMatrices<int>(inputMessage.getContent());
-        // Multiply the matrices
-        Matrix<int> result = matrices[0] * matrices[1];
+    // Deserialize the matrices
+    vector<Matrix<T>> matrices = Serializer::unserializeMatrices<T>(inputMessage.getContent());
+    // Multiply the matrices
+    Matrix<T> result = matrices[0] * matrices[1];
 
-        // Check the actual step of the computation
-        int *stepCounter;
-        if ((stepCounter = GlobalContext<int>::get(globalId)) != nullptr) {
-            // The computation is going-on
-            // Add the result to the stored matrix
-            Matrix<int> *storedMatrix = GlobalContext<Matrix<int>>::get(globalId);
-            (*storedMatrix) += result;
-            // Increment the actual step
-            (*stepCounter)++;
-        } else {
-            // The computation starts
-            // Store the result matrix
-            GlobalContext<Matrix<int>>::registerObject(globalId, result); // TODO: Check if it creates a copy
-            // Create and store the step counter
-            GlobalContext<int>::registerObject(globalId, 1);
-        }
-
-        Message outputMessage;
-        // Check if the computation is finished
-        if (stepCounter != nullptr && steps == (*stepCounter)) {
-            // Send the result
-            outputMessage = ResponseBuilder::matrixMultiplicationResultFragmentMessage(calculationId, taskId, startRow,startCol, result);
-
-            // Empty the GlobalContext
-            GlobalContext<int>::unregisterObject(globalId);
-            GlobalContext<Matrix<int>>::unregisterObject(globalId);
-        } else
-        {
-            // Send a acknowledgement message
-            outputMessage = ResponseBuilder::heartbeatMessage();
-        }
-
-        // Send the response
-        response = outputMessage.toCoapMessage(session);
-
-    } else if (matrixType == typeid(double).name()) {
-        vector<Matrix<double>> matrices = Serializer::unserializeMatrices<double>(inputMessage.getContent());
-        // TODO: Duplicate code here
-    } else if (matrixType == typeid(char).name()) {
-        vector<Matrix<char>> matrices = Serializer::unserializeMatrices<char>(inputMessage.getContent());
-        // TODO: And here
+    // Check the actual step of the computation
+    int *stepCounter;
+    if ((stepCounter = GlobalContext<int>::get(globalId)) != nullptr) {
+        // The computation is going-on
+        // Add the result to the stored matrix
+        Matrix<T> *storedMatrix = GlobalContext<Matrix<T>>::get(globalId);
+        (*storedMatrix) += result;
+        // Increment the actual step
+        (*stepCounter)++;
+    } else {
+        // The computation starts
+        // Store the result matrix
+        GlobalContext<Matrix<T>>::registerObject(globalId, result); // TODO: Check if it creates a copy
+        // Create and store the step counter
+        GlobalContext<int>::registerObject(globalId, 1);
     }
+
+    Message outputMessage;
+    // Check if the computation is finished
+    if (stepCounter != nullptr && steps == (*stepCounter)) {
+        // Send the result
+        outputMessage = ResponseBuilder::matrixMultiplicationResultFragmentMessage(calculationId, taskId, startRow,startCol, result);
+
+        // Empty the GlobalContext
+        GlobalContext<int>::unregisterObject(globalId);
+        GlobalContext<Matrix<T>>::unregisterObject(globalId);
+    } else
+    {
+        // Send a acknowledgement message
+        outputMessage = ResponseBuilder::heartbeatMessage();
+    }
+
+    // Send the response
+    response = outputMessage.toCoapMessage(session);
 }
