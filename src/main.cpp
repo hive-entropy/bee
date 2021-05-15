b72968d1bb412ae6591528a05d41fbc6424bb1c2
SevenMoor <au.ottaviano@yahoo.com>
2021-05-15 18:28:58 +0200

    Debug: Printed IP address from parameter

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,12 @@
 
 
 int main(int argc, char* argv[]) {
-    /*
-    float a[] = {1,2,3,4,5,6,7,8,9,
-                        1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,
-                        9,2,7,4,5,6,3,8,1};
-    Matrix<float> A(9,9,a);
-    Matrix<float> B(9,9,MatrixArchetype::ONES);
-    Matrix<float> C(9,9,MatrixArchetype::ZEROS);
-*/
     // Dependency injection
     HiveEntropyNode *hiveEntropyNode;
-    if(argc>=2)
+    if(argc>=2){
+        cout << "Found IP address "+std::string(argv[1])+" to use" << endl;
         hiveEntropyNode = new  HiveEntropyNode(std::string(argv[1])+":9999");
+    }
     else
          hiveEntropyNode = new  HiveEntropyNode("127.0.0.1:9999");
 
@@ -33,23 +27,6 @@ int main(int argc, char* argv[]) {
     std::cout << "Starting worker" << std::endl;
     bee.run();
     std::cout << "Worker ready" << std::endl;
-/*
-    hiveEntropyNode.queryNodeAvailability();
-    
-    std::string id="potato";
-    GlobalContext<Matrix<float>>::registerObject(id, C);
-
-    cout << "Sending..." << endl;
-    MessageHandler::divideCalculInTasks(&hiveEntropyNode, A, B, id);
-
-    cout << "result from nodes " << endl; 
-    Matrix<float>* total = GlobalContext<Matrix<float>>::get("result"+id);
-    total->show();
-    cout << "result " << endl;
-    C = A*B;
-    C.show();
-    cout << "end" << endl;
-    */
     hiveEntropyNode->keepAlive();
 
     return 0;
